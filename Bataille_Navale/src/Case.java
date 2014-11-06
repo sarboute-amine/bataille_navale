2d9ab6361ac730d5efdb1293dd209875c9e39522
zahir <zahir_badr@hotmail.fr>
2014-11-06 01:33:59 +0100

    test2

--- Bataille_Navale/src/Case.java
+++ Bataille_Navale/src/Case.java
@@ -1,6 +1,15 @@
 
 public class Case {
-	int id_case;
-	int x,y;
+	int id_case,x,y;
+	boolean e_bat, e_case;
+	
+	public Case(int id_case, int x, int y, boolean e_bat, boolean e_case) {
+		super();
+		this.id_case = id_case;
+		this.x = x;
+		this.y = y;
+		this.e_bat = e_bat;
+		this.e_case = e_case;
+	}
 
 }
--- Bataille_Navale/src/Grille.java
+++ Bataille_Navale/src/Grille.java
@@ -1,4 +1,75 @@
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+
 public class Grille {
+	
+	ArrayList<Case> grille = new ArrayList<Case>();
+	int i,j,c=0;
+	
+	 void construire_bat(Case c,int id_bat){
+		if (c.id_case == -1){
+				c.id_case=id_bat;
+			}
+			
+		}
+		
+		//constuction de la grille
+	void construire_grille(){
+		for(i=0; i<10; i++){
+			for(j=0; j<10; j++){
+				grille.add(new Case(-1,i,j,false,false));
+			}
+		}
+	}
+		 //parcourire la grille
+	void afficher_grille(){
+		for(Case elem : grille){
+			System.out.print(elem.id_case+"\t"+elem.x +"\t"+ elem.y+"|\t");
+			c++;
+			if(c==10){
+				System.out.println();
+				c=0;
+			}
+		}
+	}
+		
+	/*	// construction d'un bateau :
+		int y;
+		int num_case = 0;
+		
+		//horizentale		
+		for(y=0; y<2 ; y++){
+		construire_bat(grille.get(num_case),1);
+		num_case++;
+		}
+		//}
+		
+		//verticale
+		for(y=0; y<2 ; y++){
+			construire_bat(grille.get(num_case),1);
+			num_case=num_case+10;
+			}
+		}
+		
+		
+		System.out.println("*********** LA NOUVELLE GRILLE ***********");
+		
+		for(Case elem : grille){
+			System.out.print(elem.id_case+"\t"+elem.x +"\t"+ elem.y+"|\t");
+			c++;
+			if(c==10
+					){
+				System.out.println();
+				c=0;
+			}
+			
+		}
+		
+		
+	}*/
 
 }
