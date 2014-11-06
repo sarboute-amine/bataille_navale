15522550f46103feed2d62acc7490ddd3df7c626
zahir <zahir_badr@hotmail.fr>
2014-11-06 02:04:12 +0100

    test reussi

--- Bataille_Navale/src/Grille.java
+++ Bataille_Navale/src/Grille.java
@@ -10,21 +10,21 @@ public class Grille {
 	ArrayList<Case> grille = new ArrayList<Case>();
 	int i,j,c=0;
 	
-	 void construire_bat(Case c,int id_bat){
-		if (c.id_case == -1){
-				c.id_case=id_bat;
-			}
-			
-		}
-	 
-		//constuction de la grille
-	void construire_grille(){
+	
+	
+	 public Grille(ArrayList<Case> grille, int i, int j) {
+		super();
+		this.grille = grille;
+		this.i = i;
+		this.j = j;
+		
 		for(i=0; i<10; i++){
 			for(j=0; j<10; j++){
 				grille.add(new Case(-1,i,j,false,false));
 			}
 		}
 	}
+	 
 		 //parcourire la grille
 	void afficher_grille(){
 		for(Case elem : grille){
@@ -37,39 +37,5 @@ public class Grille {
 		}
 	}
 		
-	/*	// construction d'un bateau :
-		int y;
-		int num_case = 0;
-		
-		//horizentale		
-		for(y=0; y<2 ; y++){
-		construire_bat(grille.get(num_case),1);
-		num_case++;
-		}
-		//}
-		
-		//verticale
-		for(y=0; y<2 ; y++){
-			construire_bat(grille.get(num_case),1);
-			num_case=num_case+10;
-			}
-		}
-		
-		
-		System.out.println("*********** LA NOUVELLE GRILLE ***********");
-		
-		for(Case elem : grille){
-			System.out.print(elem.id_case+"\t"+elem.x +"\t"+ elem.y+"|\t");
-			c++;
-			if(c==10
-					){
-				System.out.println();
-				c=0;
-			}
-			
-		}
-		
-		
-	}*/
 
 }
