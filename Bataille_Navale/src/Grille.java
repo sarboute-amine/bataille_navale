2926a50188d04b6440aff51822278ed94d6218c5
zahir <zahir_badr@hotmail.fr>
2014-11-06 02:06:54 +0100

    main ajoutée

--- Bataille_Navale/src/Grille.java
+++ Bataille_Navale/src/Grille.java
@@ -1,8 +1,5 @@
 
 import java.util.ArrayList;
-import java.util.Scanner;
-
-
 
 
 public class Grille {
---
+++ Bataille_Navale/src/main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Case> grille = new ArrayList<Case>();
+		int i=0 ,j=0 ;
+		Grille g = new Grille(grille,i,j);
+		g.afficher_grille();
+
+	}
+
+}
